6430c050edd6b20601b2eb2da8b5228e1ab7c9c6
Arthur Poiret <contact@arthurpoiret.fr>
2020-08-28 18:31:34 +0200

    dev: save current values to local storage scoped by tabs

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 console.log('Starting Extension');
 
 /**
+ * Bootsrap extension state and sync UI components display with 
+ * local state values.
+ */
+async function initPopup() {
+  browser.tabs.query({ active: true, currentWindow: true })
+    .then(async function (tabs) {
+      let tabId = tabs[0].id;
+      let state = await browser.storage.local.get('tabsState');
+      let tabsState = state.tabsState;
+      console.log(tabsState)
+
+      if (!tabsState) {
+        console.log("init full state");
+        tabsState = {};
+      }
+      console.log(tabsState[tabId]);
+      if (!(tabId in tabsState)) {
+        console.log("init tab state");
+        tabsState[tabId] = {onoff: false, drywet: 0 };
+        browser.storage.local.set({tabsState: tabsState});
+      }
+      document.getElementById('onoff').checked = tabsState[tabId].onoff;
+      document.getElementById('drywet').value = tabsState[tabId].drywet;
+
+    })
+    .catch((error) => {
+      console.log('error', error);
+    });
+
+}
+
+/**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
@@ -9,6 +41,14 @@ async function initListeners() {
   async function toggleOnOff(event) {
     browser.tabs.query({ active: true, currentWindow: true })
       .then(function (tabs) {
+        browser.storage.local.get('tabsState').then(function (state) {
+          let tabId = tabs[0].id;
+          if (tabId in state.tabsState) {
+            state.tabsState[tabId].onoff = event.target.checked;
+            browser.storage.local.set({tabsState: state.tabsState});
+          }
+        });
+
         browser.tabs.sendMessage(tabs[0].id, {
           command: "updateOnOff",
           value: event.target.checked
@@ -22,6 +62,14 @@ async function initListeners() {
   async function updateDryWet(event) {
     browser.tabs.query({ active: true, currentWindow: true })
       .then(function (tabs) {
+        browser.storage.local.get('tabsState').then(function (state) {
+          let tabId = tabs[0].id;
+          if (tabId in state.tabsState) {
+            state.tabsState[tabId].drywet = event.target.value;
+            browser.storage.local.set({tabsState: state.tabsState});
+          }
+        });
+
         browser.tabs.sendMessage(tabs[0].id, {
           command: "updateDryWet",
           value: event.target.value
@@ -48,5 +96,6 @@ function reportExecuteScriptError(error) {
  * If we couldn't inject the script, handle the error.
  */
 browser.tabs.executeScript({ file: "/content_scripts/audioContext.js" })
+  .then(initPopup)
   .then(initListeners)
   .catch(reportExecuteScriptError);
